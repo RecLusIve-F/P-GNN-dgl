028137f875fa0c541cee1843f66de15a1e04116b
RecLusIve-F <1365004806@qq.com>
2022-01-18 23:21:47 +0800

    Update main.py

--- main.py
+++ main.py
@@ -151,6 +151,8 @@ def main():
             loss_func = nn.BCEWithLogitsLoss()
             out_act = nn.Sigmoid()
 
+            best_auc_val = -1
+            best_auc_test = -1
             for epoch in range(args.epoch_num):
                 if epoch == 200:
                     for param_group in optimizer.param_groups:
@@ -163,15 +165,19 @@ def main():
 
                 train_model(data_list[:effective_len], model, args, loss_func, optimizer, device)
 
-                if epoch % args.epoch_log == 0:
-                    loss_train, auc_train, loss_val, auc_val, loss_test, auc_test = eval_model(data_list, model,
-                                                                                               loss_func, out_act,
-                                                                                               device)
-                    print(repeat, epoch, 'Loss {:.4f}'.format(loss_train), 'Train AUC: {:.4f}'.format(auc_train),
-                          'Val AUC: {:.4f}'.format(auc_val), 'Test AUC: {:.4f}'.format(auc_test))
+                loss_train, auc_train, loss_val, auc_val, loss_test, auc_test = eval_model(data_list, model, loss_func,
+                                                                                           out_act, device)
 
-                    result_val.append(auc_val)
-                    result_test.append(auc_test)
+                if epoch % args.epoch_log == 0:
+                    if auc_val > best_auc_val:
+                        best_auc_val = auc_val
+                        best_auc_test = auc_test
+                    print(repeat, epoch, 'Loss {:.4f}'.format(loss_train), 'Train AUC: {:.4f}'.format(auc_train),
+                          'Val AUC: {:.4f}'.format(auc_val), 'Test AUC: {:.4f}'.format(auc_test),
+                          'Best Val AUC: {:.4f}'.format(best_auc_val), 'Best Test AUC: {:.4f}'.format(best_auc_test))
+
+                result_val.append(auc_val)
+                result_test.append(auc_test)
 
             result_val = np.array(result_val)
             result_test = np.array(result_test)
